26201c0aa11c4655648ecfc290688cba622b16e8
WSPartain <110999745+WSPartain@users.noreply.github.com>
2024-10-29 17:56:19 -0500

    motorUnlocked

--- Neo-Swerve/src/main/java/frc/robot/Constants.java
+++ Neo-Swerve/src/main/java/frc/robot/Constants.java
@@ -95,7 +95,7 @@ public class Constants {
     public static final boolean CANCODER_INVERSION = false;
 
     /** Idle modes. */
-    public static final IdleMode DRIVE_IDLE_MODE = IdleMode.kBrake;
+    public static final IdleMode DRIVE_IDLE_MODE = IdleMode.kCoast;
     public static final IdleMode ANGLE_IDLE_MODE = IdleMode.kCoast;
 
     /** 
